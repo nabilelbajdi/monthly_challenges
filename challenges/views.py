1aebef692b2c4c787e02775e4ff3ca65ec0b9f61
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2024-10-31 16:10:20 +0100

    Add index view and dynamic URL path for months; update error handling and HTML response formatting

--- challenges/views.py
+++ challenges/views.py
@@ -21,24 +21,37 @@ monthly_challenges = {
 
 # Create your views here.
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
 
-def monthly_challenge_by_number(requests, month):
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
+def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
     if month < 1 or month > 12:
-        return HttpResponseNotFound("Invalid month")
+        return HttpResponseNotFound("<h1>Invalid month!</h1>")
 
     redirect_month = months[month - 1]
-    redirect_path = reverse("month_challenge", args=[redirect_month])
+    redirect_path = reverse("month-challenge", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
 
 
-def monthly_challenge(requests, month):
+def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>Invalid month!</h1>")
 
 # So, when you type /challenges/january, Django:
 # - Routes to challenges.urls to interpret the january part as month.
